d40fa89d86beb15518f34ca026b8e2061cc3b1b8
alexa <allexa.msv@gmail.com>
2023-07-05 00:09:34 +0300

    Implemented URLSession extension

---
+++ YanDo/Extensions/URLSessionExtension .swift
@@ -0,0 +1,42 @@
+//
+//  URLSessionExtension .swift
+//  YanDo
+//
+//  Created by Александра Маслова on 04.07.2023.
+//
+
+import Foundation
+import CocoaLumberjackSwift
+
+import CocoaLumberjack
+
+extension URLSession {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        return try await withCheckedThrowingContinuation({ continuation in
+            // Создание задачи для сетевого запроса с использованием переданного URLRequest
+            let task = self.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    DDLogError("Error occurred during network request: \(error.localizedDescription)")
+                    continuation.resume(throwing: error)
+                }
+                guard let data = data, let response = response else {
+                    // Создание и запись сообщения об отсутствии данных или ответа
+                    let error = NSError(domain: "URLSession", code: 0, userInfo: [NSLocalizedDescriptionKey: "No response or data received"])
+                    DDLogError("No response or data received during network request")
+                    continuation.resume(throwing: error)
+                    return
+                }
+                // Асинхронное возобновление выполнения с полученными данными и ответом
+                DispatchQueue.main.async {
+                    continuation.resume(returning: (data, response))
+                }
+            }
+            // Проверяем, была ли задача отменена
+            if Task.isCancelled {
+                task.cancel()
+            } else {
+                task.resume()
+            }
+        })
+    }
+}
